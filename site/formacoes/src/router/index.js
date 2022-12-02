9e34448e7f25cf8d77e1bb789e498e36d20f35ec
Leonardo Franco <leonardofranco@ipvc.pt>
2022-12-02 14:22:57 +0000

    view routing

--- site/formacoes/src/router/index.js
+++ site/formacoes/src/router/index.js
@@ -8,11 +8,63 @@ const router = createRouter({
     routes: [
         {
             path: '/',
+            component: Landing
+        },
+        {
+            path: '/home',
             component: Home
         },
         {
-            path: '/about',
-            component: About
+            path: '/courses/:id',
+            component: CourseDetails
+        },
+        {
+            path: '/courselist',
+            component: CourseListAdm
+        },
+        {
+            path: '/courses',
+            component: Course
+        },
+        {
+            path: '/creators/:id',
+            component: Creator
+        },
+        {
+            path: '/studio',
+            component: CreatorStudio
+        },
+        {
+            path: '/login',
+            component: Login
+        },
+        {
+            path: '/register',
+            component: Register
+        },
+        {
+            path: '/users/:id',
+            component: Profile
+        },
+        {
+            path: '/users/:id/courses',
+            component: MyCourses
+        },
+        {
+            path: '/users/:id/diplomas',
+            component: MyDiplomas
+        },
+        {
+            path: '/settings',
+            component: Settings
+        },
+        {
+            path: '/courses/:id/videos/:id',
+            component: VideoPlayer
+        },
+        {
+            path: '/users',
+            component: UserListAdm
         }
     ]
 })
