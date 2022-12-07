1d84b1f2038e136597e4c2117791ed76cd65cb7b
leonardomarrao <61155148+leonardomarrao@users.noreply.github.com>
2022-12-07 16:45:03 +0000

    LandingPage_TopBar

--- site/formacoes/src/components/Topbar.vue
+++ site/formacoes/src/components/Topbar.vue
@@ -1,6 +1,21 @@
 <template>
-    <div> 
-        <v-card text="Lorem ipsum dolor sit amet consectetur adipisicing elit. Commodi, ratione debitis quis est labore voluptatibus! Eaque cupiditate minima, at placeat totam, magni doloremque veniam neque porro libero rerum unde voluptatem!" variant="tonal"></v-card>
+    <div class="topBar"> 
+        <div class="logo">
+            <router-link class="topBarButton" to="/">
+                <span class="text">Formação Johnson</span>
+            </router-link>
+
+        </div>
+        <div class="rightSide">
+            <router-link class="topBarButton" to="/">
+                <span class="text">Cursos</span>
+            </router-link>
+
+            <input class="searchBox" type="text" placeholder="Pesquisar cursos..."/>
+            <router-link class="topBarButton" to="/">
+                <span class="text">Login</span>
+            </router-link>
+        </div>
     </div>
 </template>
 
@@ -10,6 +25,26 @@ export default {
 }
 </script>
 
-<style>
-
+<style scoped>
+.topBar {
+    width: 100%;
+    height: fit-content;
+    padding-top: 22px;
+    padding-left: 10px;
+    padding-right: 10px;
+    display: flex;
+    justify-content: space-between;
+}
+.logo {
+    font-size: 35px;
+}
+.rightSide {
+    width: 400px;
+    display: flex;
+    justify-content: space-between;
+    font-size: 30px;
+}
+.topBarButton {
+    text-decoration: none;
+}
 </style>
