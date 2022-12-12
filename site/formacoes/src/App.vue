c635dc306fbbdce0bd1942ee4eba4213caaef74c
leonardomarrao <61155148+leonardomarrao@users.noreply.github.com>
2022-12-12 15:07:55 +0000

    packages

--- site/formacoes/src/App.vue
+++ site/formacoes/src/App.vue
@@ -1,8 +1,8 @@
 <template>
   <div class="app">
-    <Sidebar/>
-    <Topbar/>
+    <!--adicionar topbar?-->
     <router-view/>
+	<!--adicionar o sidebar em cima do topbar-->
   </div>
 </template>
 
