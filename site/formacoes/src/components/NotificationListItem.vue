f6d25c6a5c3142f69d8650041d36f0db2ac6d71a
Leonardo Franco <leonardofranco@ipvc.pt>
2022-12-15 14:51:33 +0000

    notifications (temp on landing topbar)

---
+++ site/formacoes/src/components/NotificationListItem.vue
@@ -0,0 +1,44 @@
+<template>
+    <div class="notification-wrapper">
+        <img :src="this.imageUrl">
+        <p>{{ this.notification.message }}</p>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'NotificationListItem',
+    props: {
+        notification: {
+            type: Object,
+            required: true
+        }
+    },
+    data(){
+        return {
+            imageUrl: ""
+        }
+    },
+    created(){
+        this.imageUrl = new URL(`../assets/${this.notification.image}.jpg`, import.meta.url).href;
+    }
+}
+</script>
+
+<style scoped>
+    .notification-wrapper {
+        display: flex;
+        gap: 16px;
+        padding: 8px;
+        margin: 8px 0px;
+
+        background-color: grey;
+    }
+
+    .notification-wrapper img {
+        width: 48px;
+        height: 48px;
+        object-fit: cover;
+        border-radius: 50%;
+    }
+</style>
