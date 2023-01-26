bf0fa68d3238648eadf91065b7a773d72ccc3ffa
leonardomarrao <61155148+leonardomarrao@users.noreply.github.com>
2023-01-26 15:02:10 +0000

    notificacao no video e encriptacao

--- site/backend/services/user.js
+++ site/backend/services/user.js
@@ -145,7 +145,7 @@ async function createUser(user) {
         dbUser.isUsernameTaken(user.username).then(async value => {
             if (value.length == 0) {
 
-                dbUser.getAllIDs().then(value2 => {
+                dbUser.getAllIDs().then(async value2 => {
                     
                     let id;
                     let existe;
@@ -158,6 +158,8 @@ async function createUser(user) {
                             if(i.id == id) existe = true;
                         });
                     } while(existe)
+
+                    user.password = await bcrypt.hash(user.password, 10); //encriptacao---> depois ver se é ok essa linha obrigar a funçao que ela pertence ser async
 
                     dbUser.createUser(id, user).then(value => {
                         resolve({ code: 201, info: { message: "User registado com sucesso."} });
