d3522b58b71ea4bddb3a699b61ef7973c7a1f82a
dre-franco <andreffranco98@gmail.com>
2023-01-30 17:10:19 +0000

    funcao pain funciona

--- site/backend/db/criador.js
+++ site/backend/db/criador.js
@@ -20,7 +20,7 @@ async function getAllCriadores() {
     return new Promise((resolve, reject) => {
         const slct = `SELECT * FROM [Users] where [type] = 'creator'`;
         pool.query(slct, (err, res) => {
-            if(!err) {
+            if (!err) {
                 resolve(res.recordset);
             } else {
                 reject(err.message);
@@ -73,8 +73,53 @@ async function changeCriadorState(state, id) {
     return new Promise((resolve, reject) => {
         const updt = `UPDATE [Users] SET [state] = '${state}' WHERE [id] = '${id}' AND [type] = 'creator'`;
         pool.query(updt, (err, res) => {
-            if(!err) {
+            if (!err) {
                 resolve(res);
+            } else {
+                reject(err.message);
+            }
+        });
+    });
+}
+
+async function getSubsCriador(id) {
+    return new Promise((resolve, reject) => {
+        const slct = `SELECT * FROM [Subscription] WHERE [id_subscribed] = '${id}'`;
+        pool.query(slct, (err, res) => {
+            if (!err) {
+                resolve(res.recordset);
+            } else {
+                reject(err.message);
+            }
+        });
+    });
+}
+
+async function getPointsCompra() {
+    return new Promise((resolve, reject) => {
+        const slct = `SELECT u.id AS id, u.username AS username, u.name AS name, u.image AS image, count(uc.id) AS points
+        FROM Users u LEFT JOIN Course c on u.id = c.id_creator LEFT JOIN User_Course uc on uc.id_course = c.id
+        WHERE u.state = 'Ativo'
+        GROUP BY u.id, u.username, u.name, u.image, u.type`;
+        pool.query(slct, (err, res) => {
+            if (!err) {
+                resolve(res.recordset);
+            } else {
+                reject(err.message);
+            }
+        });
+    });
+}
+
+async function getPointsSubs() {
+    return new Promise((resolve, reject) => {
+        const slct = `SELECT u.id AS id, u.username AS username, u.name AS name, u.image AS image, count(s.id_subscribed) * 3 AS points
+        FROM Users u LEFT JOIN Subscription s on u.id = s.id_subscribed
+        WHERE state = 'Ativo' AND s.final_date is null
+        GROUP BY u.id, u.username, u.name, u.image, u.type`;
+        pool.query(slct, (err, res) => {
+            if (!err) {
+                resolve(res.recordset);
             } else {
                 reject(err.message);
             }
@@ -87,5 +132,9 @@ module.exports = {
     getCriador: getCriador,
     createCriador: createCriador,
     updateCriador: updateCriador,
-    changeCriadorState: changeCriadorState
+    changeCriadorState: changeCriadorState,
+    getSubsCriador: getSubsCriador,
+    getPointsCompra: getPointsCompra,
+    getPointsSubs: getPointsSubs,
+    //getCriadoresHomepage: getCriadoresHomepage,
 }
--- site/backend/services/criador.js
+++ site/backend/services/criador.js
@@ -14,39 +14,39 @@ async function getAllCriadores(tokens) {
                     info.admins = value2;
                     resolve({ code: 200, info: info });
                 })
-                .catch(error => {
-                    console.log(error);
-                    reject({ code: 400, error: { message: "Algo correu mal com a query." } });
-                })
+                    .catch(error => {
+                        console.log(error);
+                        reject({ code: 400, error: { message: "Algo correu mal com a query." } });
+                    })
             } else {
                 reject({ code: 403, error: { message: "O user que tentou completar essa ação não é administrador." } });
             }
         })
-        .catch(error => {
-            console.log(error);
-            reject({ code: 401, error: { message: "Token inválido." } });
-        })
+            .catch(error => {
+                console.log(error);
+                reject({ code: 401, error: { message: "Token inválido." } });
+            })
     })
 }
 
 async function getCriador(headers, id) {
     return new Promise((resolve, reject) => {
-        
+
         let access_token;
         let refresh_token;
 
-        if(headers['authorization']) {
+        if (headers['authorization']) {
             access_token = headers['authorization'].split(' ')[1];
             refresh_token = headers.refreshtoken;
         }
 
-        if(access_token && refresh_token) {
+        if (access_token && refresh_token) {
 
             utils.validateToken(access_token, refresh_token).then(value => {
 
                 dbCria.getCriador(id).then(value2 => {
-                    if(value2.length == 0) {
-                        reject({ code: 404, error: { message: "Este criador não existe."}});
+                    if (value2.length == 0) {
+                        reject({ code: 404, error: { message: "Este criador não existe." } });
                     } else {
                         dbCour.getCursosByCriador(id).then(value3 => {
                             let resp = {
@@ -58,26 +58,26 @@ async function getCriador(headers, id) {
 
                             resolve({ code: 200, info: resp });
                         })
-                        .catch(error => {
-                            console.log(error);
-                            reject({ code: 400, error: {message: "Algo correu mal com a query."}});
-                        });
+                            .catch(error => {
+                                console.log(error);
+                                reject({ code: 400, error: { message: "Algo correu mal com a query." } });
+                            });
                     }
                 })
+                    .catch(error => {
+                        console.log(error);
+                        reject({ code: 400, error: { message: "Algo correu mal com a query." } });
+                    });
+            })
                 .catch(error => {
                     console.log(error);
-                    reject({ code: 400, error: {message: "Algo correu mal com a query."}});
+                    reject({ code: 401, error: { message: "Token inválido." } })
                 });
-            })
-            .catch(error => {
-                console.log(error);
-                reject({ code: 401, error: {message: "Token inválido."}})
-            });
 
         } else {
             dbCria.getCriador(id).then(value => {
-                if(value.length == 0){
-                    reject({ code: 404, error: { message: "Este criador não existe."}});
+                if (value.length == 0) {
+                    reject({ code: 404, error: { message: "Este criador não existe." } });
                 } else {
                     dbCour.getCursosByCriador(id).then(value2 => {
                         let criador = value[0];
@@ -85,72 +85,72 @@ async function getCriador(headers, id) {
 
                         resolve({ code: 200, info: { criador: value[0] } });
                     })
-                    .catch(error => {
-                        console.log(error);
-                        reject({ code: 400, error: {message: "Algo correu mal com a query."}});
-                    });
+                        .catch(error => {
+                            console.log(error);
+                            reject({ code: 400, error: { message: "Algo correu mal com a query." } });
+                        });
                 }
             })
-            .catch(error => {
-                console.log(error);
-                reject({ code: 400, error: {message: "Algo correu mal com a query."}});
-            });
-        } 
+                .catch(error => {
+                    console.log(error);
+                    reject({ code: 400, error: { message: "Algo correu mal com a query." } });
+                });
+        }
     });
 }
 
 async function createCriador(tokens, criador) {
     return new Promise((resolve, reject) => {
         utils.validateToken(tokens.access_token, tokens.refresh_token).then(value => {
-            if(value.user.type == 'admin') {
+            if (value.user.type == 'admin') {
 
                 dbUser.isUsernameTaken(criador.username).then(async value2 => {
                     if (value2.length == 0) {
-        
+
                         dbUser.getAllIDs().then(value3 => {
                             let id;
                             let existe;
-        
+
                             do {
                                 id = uuid.v4();
                                 existe = false;
-            
+
                                 value3.forEach(u => {
-                                    if(u.id == id) existe = true;
+                                    if (u.id == id) existe = true;
                                 });
-                            } while(existe)
-        
+                            } while (existe)
+
                             dbCria.createCriador(id, criador).then(value4 => {
                                 let info = value;
                                 info.message = "Criador criado com sucesso."
-                                resolve({ code: 201, info: info});
+                                resolve({ code: 201, info: info });
                             })
+                                .catch(error => {
+                                    console.log(error);
+                                    reject({ code: 400, error: { message: "Algo correu mal com a query de insert." } });
+                                });
+                        })
                             .catch(error => {
                                 console.log(error);
-                                reject({ code: 400, error: { message: "Algo correu mal com a query de insert." }});
+                                reject({ code: 400, error: { message: "Algo correu mal com a query." } });
                             });
-                        })
-                        .catch(error => {
-                            console.log(error);
-                            reject({ code: 400, error: { message: "Algo correu mal com a query." }});
-                        });
                     } else {
-                        reject({ code: 400, error: { message: 'Já tem um user com esse username.' }});
+                        reject({ code: 400, error: { message: 'Já tem um user com esse username.' } });
                     }
                 })
-                .catch(error => {
-                    console.log(error);
-                    reject({ code: 401, error: { message: 'Erro na query.' }});
-                });
+                    .catch(error => {
+                        console.log(error);
+                        reject({ code: 401, error: { message: 'Erro na query.' } });
+                    });
 
             } else {
                 reject({ code: 403, error: { message: "O user que tentou completar essa ação não é administrador." } });
             }
         })
-        .catch(error => {
-            console.log(error);
-            reject({ code: 401, error: { message: "Token inválido." } });
-        })
+            .catch(error => {
+                console.log(error);
+                reject({ code: 401, error: { message: "Token inválido." } });
+            })
     });
 }
 
@@ -161,35 +161,35 @@ async function changeCriadorState(tokens, id, criador) {
             if (info.user.type == 'admin') {
 
                 dbCria.getCriador(id).then(value2 => {
-                    if(value2.length == 0) {
-                        reject({ code: 404, error: { message: "Este utilizador não existe." }});
+                    if (value2.length == 0) {
+                        reject({ code: 404, error: { message: "Este utilizador não existe." } });
                     } else {
-                        if(criador.state == "Inativo" || criador.state == "Ativo") {
+                        if (criador.state == "Inativo" || criador.state == "Ativo") {
                             dbCria.changeCriadorState(criador.state, id).then(value => {
                                 info.message = "Estado alterado com sucesso.";
                                 resolve({ code: 200, info: info });
                             })
-                            .catch(error => {
-                                console.log(error);
-                                reject({ code: 400, error: { message: "Algo correu mal com a query." }});
-                            });
+                                .catch(error => {
+                                    console.log(error);
+                                    reject({ code: 400, error: { message: "Algo correu mal com a query." } });
+                                });
                         } else {
-                            reject({ code: 400, error: { message: "Estado inválido." }});
+                            reject({ code: 400, error: { message: "Estado inválido." } });
                         }
                     }
                 })
-                .catch(error => {
-                    console.log(error);
-                    reject({ code: 400, error: { message: "Algo correu mal com a query." }});
-                });
+                    .catch(error => {
+                        console.log(error);
+                        reject({ code: 400, error: { message: "Algo correu mal com a query." } });
+                    });
             } else {
-                reject({ code: 403, error: { message: "A operação não foi possível porquê o user não é um administrador." }});
+                reject({ code: 403, error: { message: "A operação não foi possível porquê o user não é um administrador." } });
             }
         })
-        .catch(error => {
-            console.log(error);
-            reject({ code: 401, error: { message: "Token inválido." }});
-        });
+            .catch(error => {
+                console.log(error);
+                reject({ code: 401, error: { message: "Token inválido." } });
+            });
     });
 }
 
@@ -199,25 +199,62 @@ async function updateCriador(tokens, id, criador) {
             let info = value;
             if (info.user.id == id) {
                 if (criador.name == "" || criador.image == "" || criador.price == "" || criador.name == null || criador.image == null || criador.price == null || criador.description == null || criador.price < 0) {
-                    reject({ code: 400, error: { message: "A alteração não pode ser feita, porque há valores vazios/inválidos." }});
+                    reject({ code: 400, error: { message: "A alteração não pode ser feita, porque há valores vazios/inválidos." } });
                 } else {
                     dbCria.updateCriador(criador, id).then(value => {
                         info.message = "Criador alterado com sucesso.";
                         resolve({ code: 200, info: info });
                     })
-                    .catch(error => {
-                        console.log(error);
-                        reject({ code: 400, error: { message: "Algo correu mal com a query." }});
-                    });
+                        .catch(error => {
+                            console.log(error);
+                            reject({ code: 400, error: { message: "Algo correu mal com a query." } });
+                        });
                 }
             } else {
-                reject({ code: 403, error: { message: "A operação não foi possível porquê o user associado ao token não é o mesmo a qual estas a tentar fazer update." }});
+                reject({ code: 403, error: { message: "A operação não foi possível porquê o user associado ao token não é o mesmo a qual estas a tentar fazer update." } });
             }
         })
-        .catch(error => {
+            .catch(error => {
+                console.log(error);
+                reject({ code: 401, error: { message: "Token inválido." } });
+            });
+    });
+}
+
+async function getCriadoresHomepage() {
+    return new Promise((resolve, reject) => {
+
+        let promises = []
+        let creators = []
+
+        promises.push(dbCria.getPointsCompra())
+        promises.push(dbCria.getPointsSubs())
+
+        Promise.all(promises).then(values => {
+
+            creators = values[0]
+
+            values[1].forEach(val => {
+                creators.forEach(c => {
+                    if (c.id === val.id) {
+                        c.points += val.points
+                    }
+                })
+            })
+
+            creators.sort((a, b) => (a.points < b.points) ? 1 : ((b.points < a.points) ? -1 : 0))
+
+            creators = creators.slice(0, 10);
+
+            resolve({ code: 200, info: creators });
+
+        }).catch(error => {
             console.log(error);
-            reject({ code: 401, error: { message: "Token inválido." }});
+            reject({ code: 400, error: { message: "Algo correu mal com a query." } });
         });
+
+
+
     });
 }
 
@@ -226,5 +263,6 @@ module.exports = {
     getCriador: getCriador,
     createCriador: createCriador,
     updateCriador: updateCriador,
-    changeCriadorState: changeCriadorState
+    changeCriadorState: changeCriadorState,
+    getCriadoresHomepage: getCriadoresHomepage,
 }
