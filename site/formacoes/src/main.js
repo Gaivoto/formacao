249568734512062abf51888cc7e7a528f4c8ba20
Leonardo Franco <leonardofranco@ipvc.pt>
2023-02-15 14:03:39 +0000

    ajkjsd

--- site/formacoes/src/main.js
+++ site/formacoes/src/main.js
@@ -2,8 +2,13 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router/index.js'
 import store from './store/index.js'
+import i18n from './i18n'
 
 import 'bootstrap/dist/css/bootstrap.min.css'
 import 'pattern.css'
 
-createApp(App).use(router).use(store).mount('#app')
+createApp(App)
+    .use(router)
+    .use(store)
+    .use(i18n)
+    .mount('#app')
