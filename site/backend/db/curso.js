da6f2845e28264db3ccf74a35773d5d08b349951
leonardomarrao <61155148+leonardomarrao@users.noreply.github.com>
2023-02-23 13:10:03 +0000

    mandar ratings

--- site/backend/db/curso.js
+++ site/backend/db/curso.js
@@ -192,7 +192,7 @@ async function getMostSoldCursos() {
         const slct = `SELECT TOP 6 c.id as id, c.name as name, c.category as category, c.description as deescription, c.date as date, c.state as state, c.price as price, c.image as image, c.rating, u.id as idCr, u.name as nameCr, u.image as imageCr, count(uc.id) as vendas
         FROM Course c LEFT JOIN User_Course uc on uc.id_course = c.id LEFT JOIN Users u on c.id_creator = u.id
         WHERE c.state = 'Ativo'
-        GROUP BY c.id, c.name, c.category, c.description, c.date, c.state, c.price, c.image, u.id, u.name, u.image
+        GROUP BY c.id, c.name, c.category, c.description, c.date, c.state, c.price, c.image, c.rating, u.id, u.name, u.image
         ORDER BY vendas desc`;
         pool.query(slct, (err, res) => {
             if (!err) {
@@ -214,7 +214,7 @@ async function getDestaquesCursos(data) {
         INNER JOIN Users u 
         ON c.id_creator = u.id
         WHERE c.state = 'Ativo' AND uc.date_bought > @data
-        GROUP BY c.id, c.name, c.category, c.description, c.date, c.state, c.price, c.image, u.id, u.name, u.image
+        GROUP BY c.id, c.name, c.category, c.description, c.date, c.state, c.price, c.rating, c.image, u.id, u.name, u.image
         ORDER BY pointsCursos DESC`;
         pool.input('data', sql.DateTime, data).query(slct, (err, res) => {
             if (!err) {
@@ -260,7 +260,21 @@ async function rateCourse(rating, id) {
     const pool = new sql.Request();
     return new Promise((resolve, reject) => {
         const updt = `UPDATE [User_Course] SET [rating] = @rating WHERE [id] = @id`;
-        pool.input('rating', sql.Float, rating).input('id', sql.VarChar(200), 'id').query(updt, (err,res) => {
+        pool.input('rating', sql.Float, rating).input('id', sql.VarChar(200), id).query(updt, (err,res) => {
+            if (!err) {
+                resolve(res.recordset);
+            } else {
+                reject(err.message);
+            }
+        });
+    })
+}
+
+async function getUserRatingOfCourse(id_user, id_course) {
+    const pool = new sql.Request();
+    return new Promise((resolve, reject) => {
+        const slct = `SELECT [id], [rating] FROM User_Course WHERE [id_user] = @id_user AND [id_course] = @id_course`;
+        pool.input('id_user', sql.VarChar(200), id_user).input('id_course', sql.VarChar(200), id_course).query(updt, (err,res) => {
             if (!err) {
                 resolve(res.recordset);
             } else {
@@ -288,5 +302,6 @@ module.exports = {
     getRecomendedCursos: getRecomendedCursos,
     getOtherCursos: getOtherCursos,
     rateCourse: rateCourse,
+    getUserRatingOfCourse: getUserRatingOfCourse
 
 }
