61d622c2a70a260960e366ecc0d2b2114dc87ab0
Leonardo Franco <leonardofranco@ipvc.pt>
2023-02-23 13:20:12 +0000

    a

--- site/formacoes/src/views/Home.vue
+++ site/formacoes/src/views/Home.vue
@@ -106,6 +106,7 @@ export default {
             url: `${import.meta.env.VITE_HOST}/cursos/home`
         })
         .then(value => {
+            console.log(value.data)
             value.data.recomendados.forEach(c => this.recommended.push(c));
             this.slides.push({...value.data.recomendados[0]});
             value.data.destaques.forEach(c => this.hottest.push(c));
